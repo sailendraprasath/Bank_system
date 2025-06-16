b7327068bb97b40afae6eaef9a3d3f27edfcc553
sailendraprasath <sailendraprasath05@gmail.com>
2025-06-16 23:24:49 +0530

    well

--- banking.py
+++ banking.py
@@ -18,143 +18,97 @@ def ask_client():
     else:
         print("Plzz Enter Your words should be yes OR no")
         ask_client()
-data = []
-acc_no = 1001
-client_name = ""
-client_ph_num = 0
-client_email = ""
-client_address = ""
-client_pan_num = ""
-acc_type = 0
+
+client_acc_no = []
+client_name = []
+client_ph_num = []
+client_address = []
+balance = []
+acc_num = 1000
 client_initial_amt = 0
-balance = 0
+
+def gen_acc_no(val):
+    global acc_num
+    acc_num += 1
+    val = acc_num
+    client_acc_no.append(val)
+    return val
 
 def acc_creation():
     print("\t<< HERE YOUR ACCOUNT CREATION >>")
-    global client_name,client_ph_num,client_email,client_address,client_pan_num,acc_type,client_initial_amt,balance
-    client_name = input("Enter your Full Name Here: ")
-    client_ph_num = int(input("Enter your Phone Number Here: "))
-    client_email = input("Enter Your Email here: ")
-    client_address = input("Enter your Address here: ")
-    client_pan_num = input("Enter Your PAN Card Code here: ")
-    print("Account Type Option")
-    print("\t1) Savings Account")
-    print("\t2) Current Account")
-    while True:
-        acc_type = int(input("Enter Your Option here: "))
-        if acc_type == 1:
-            print("Accepted Savings Account")
-            break
-        elif acc_type == 2:
-            print("Accepted Current Account")
-            break
-        else:
-            print("Option Should be 1 or 2 Accept here")
-            print("\tPlease Try Again..!!")
+    global client_name,client_ph_num,client_address,client_initial_amt,balance
+    cus_name = input("Enter your Full Name Here: ")
+    client_name.append(cus_name)
+    cus_ph_num = int(input("Enter your Phone Number Here: "))
+    client_ph_num.append(cus_ph_num)
+    cus_address = input("Enter your Address here: ")
+    client_address.append(cus_address)
     while True:
         client_initial_amt = float(input("Enter Your Deposit Initial Amount 500 only: "))
-        if client_initial_amt >=501  or client_initial_amt <= 0:
+        if client_initial_amt != 500:
             print("Please Enter as per required Amount")
             print("Try again..!!")
         else:
-            balance += client_initial_amt
+            balance.append(client_initial_amt)
             break
     print("\tAccount Created SuccessFully")
-    print("\tYour Account number is: ",acc_no)
+    put_acc_num = gen_acc_no(acc_num)
+    print("\tYour Account number is: ",put_acc_num)
 
 
 def acc_Detail_Display():
-    if client_name == "":
-        print("\t<< UNAVAILABLE NOW >>")
-        print("Currently you dont have a account")
-        print("First enter yes to continue press 1 to Create a Account")
-    else:
-        while True:
-            acc_num = int(input("Enter your Account number here: "))
-            if acc_num == 1001:
-                break
-            else:
-                print("\t<< UNAVAILABLE NOW >>")
-                print("Please Enter correct Account number")
-                print("Try again..!!")
-        print("\t<< DISPLAY ACCOUNT DETAIL >>")
-        print("Name", end="\t\t")
-        print("AccountNo", end="\t\t")
-        print("Balance")
-        print(client_name, end="\t\t ")
-        print(acc_no, end="\t\t\t ")
-        print(balance)
+    print("\t<< HERE DISPLAY ALL ACCOUNTS >>")
+    print("Account_No", end="\t")
+    print("Acc_Holder_Name", end="\t\t")
+    print("Balance")
+    for i in range(len(client_acc_no)):
+        print(client_acc_no[i], end="\t\t")
+        print(client_name[i], end="\t\t\t")
+        print(balance[i])
 
 
 def acc_check_balance():
-    if client_name == "":
-        print("\t<< UNAVAILABLE NOW >>")
-        print("Currently you dont have a account")
-        print("First enter yes to continue press 1 to Create a Account")
+    print("\t<< CHECK BALANCE >>")
+    account_num = int(input("Enter your ACCOUNT Number: "))
+    for get in range(len(client_acc_no)):
+        if client_acc_no[get] == account_num:
+            print("Your Current Balance is ₹",balance[get])
+            print("Account Holder Name:",client_name[get])
+            break
     else:
-        print("\t<< HERE YOUR BALANCE AMOUNT >>")
-        while True:
-            acc_num = int(input("Enter your Account number here: "))
-            if acc_num == 1001:
-                break
-            else:
-                print("\t<< UNAVAILABLE NOW >>")
-                print("Please Enter correct Account number")
-                print("Try again..!!")
-        print("Account Holder name: ", client_name)
-        print("Account number:", acc_no)
-        print("Account holder Address:", client_address)
-        print("You have ₹", balance, "amount in your Account")
+        print("invalid Account number Try again..!!")
 
 def acc_deposit():
-    if client_name == "":
-        print("\t<< UNAVAILABLE NOW >>")
-        print("Currently you dont have a account")
-        print("First enter yes to continue press 1 to Create a Account")
+    account_num = int(input("Enter Your Account Number: "))
+    while True:
+        dep_amt = float(input("Enter your Deposit Amount here: "))
+        if dep_amt < 0:
+            print("Invalid amount Try again")
+        else:
+            break
+
+    for get in range(len(client_acc_no)):
+        if client_acc_no[get] == account_num:
+            balance[get]+= dep_amt
+            print("Amount ₹",dep_amt,"has been Deposited Successfully")
+            print("Your current Balance is ₹",balance[get])
+            break
     else:
-        while True:
-            acc_num = int(input("Enter your Account number here: "))
-            if acc_num == 1001:
-                global balance
-                print("\t<< HERE YOU DEPOSIT YOUR AMOUNT >>")
-                dep_amt = float(input("Enter your Deposit amount here: "))
-                balance += dep_amt
-                print("Your deposit Amount is:",dep_amt)
-                print("You have ₹", balance, "amount in your Account")
-                break
-            else:
-                print("\t<< UNAVAILABLE NOW >>")
-                print("\tPlease Enter correct Account number")
-                print("\tTry again..!!")
-
-
+        print("InCorrect Account number Please Try again..!!")
 def acc_withdraw():
-    if client_name == "":
-        print("\t<< UNAVAILABLE NOW >>")
-        print("Currently you dont have a account")
-        print("First enter yes to continue press 1 to Create a Account")
-    else:
-        while True:
-            acc_num = int(input("Enter your Account number here: "))
-            if acc_num == 1001:
-                global balance
-                print("\t<< HERE YOU WITHDRAW YOUR AMOUNT >>")
-                while True:
-                    wit_amt = float(input("Enter your Withdraw amount here: "))
-                    if wit_amt > balance or wit_amt == 0 or wit_amt <= 0:
-                        print("check your enter number")
-                        print("Try Again..!!")
-                    else: #wit_amt <= balance
-                        balance-=wit_amt
-                        print("You Withdraw Amount:",wit_amt)
-                        print("You have ₹", balance, "amount in your Account")
-                        break
-                break
+    account_num = int(input("Enter Your Account Number: "))
+    wit_amt = float(input("Enter Your Withdraw Amount: "))
+    for get in range(len(client_acc_no)):
+        if client_acc_no[get] == account_num:
+            if balance[get] >= wit_amt:
+                balance[get] -= wit_amt
+                print("Amount ₹",wit_amt,"has been Withdraw Successfully")
+                print("Your Current Balance is ₹",balance[get])
             else:
-                print("\t<< UNAVAILABLE NOW >>")
-                print("\tPlease Enter correct Account number")
-                print("\tTry again..!!")
-
+                print("Insufficient Balance ")
+            break
+    else:
+        print("InCorrect Account number Please Try again..!!")
 
 while True:
     greetings()
